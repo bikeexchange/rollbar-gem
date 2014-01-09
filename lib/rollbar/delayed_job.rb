d9ec00cf99e116c8abee6541717ab39fc116c544
Michael Pearson <mipearson@gmail.com>
2014-01-10 09:45:22 +1100

    Remove DJ integration to see if that fixes our sigsegvs

--- lib/rollbar/delayed_job.rb
+++ lib/rollbar/delayed_job.rb
@@ -1,25 +1,2 @@
 # based on http://bit.ly/VGdfVI
 
-module Delayed
-  module Plugins
-    class Rollbar < Plugin
-      module ReportErrors
-        def error(job, error)
-          # send the job object as the 'request data'
-          ::Rollbar.report_exception(error, job)
-          super if defined?(super)
-        end
-      end
-
-      callbacks do |lifecycle|
-        lifecycle.before(:invoke_job) do |job|
-          payload = job.payload_object
-          payload = payload.object if payload.is_a? Delayed::PerformableMethod
-          payload.extend ReportErrors
-        end
-      end
-    end
-  end
-end
-
-Delayed::Worker.plugins << Delayed::Plugins::Rollbar
